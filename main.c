2f900a064e8b4f2e7fd8e2b5a6496013440ac0db
DESKTOP-0OAC0ET\Chase <livingstonchase94@gmail.com>
2024-12-01 13:06:36 -0500

    changed what i wanted

--- main.c
+++ main.c
@@ -11,17 +11,53 @@ int main()
     InitWindow(screenWidth, screenHeight, "Hi Nida");
     SetTargetFPS(60);
     int measuredText = MeasureText("I love you!", 48);
+    int direction = 1;
+    int calculatedTextX = (screenWidth/2) - measuredText/2;
+    int calculatedTextY = screenHeight/2;
+    int textSize = 48;
+    int textScaleDirection = 1;
+    int rotation = 0;
+    int rotationDirection = 1;
 
     while (!WindowShouldClose())
     {
         ClearBackground(BLACK);
         BeginDrawing();
 
-        DrawText("I love you!", (screenWidth/2) - measuredText/2, screenHeight/2, 48, GREEN);
-               
+        DrawTextPro(GetFontDefault(), "I love you!", (Vector2){calculatedTextX, calculatedTextY}, (Vector2){0, 0}, rotation, textSize, 3, GREEN);
+        calculatedTextX += direction;
+        if (calculatedTextX <= 0) 
+        {
+            direction = 1;
+        } 
+        else if (calculatedTextX >= screenWidth - measuredText) 
+        {
+            direction = -1;
+        }
+        textSize += textScaleDirection;
+        if (textSize >= 108)
+        {
+            textScaleDirection = -1;
+        }
+        else if (textSize <= 24)
+        {
+            textScaleDirection = 1;
+        }
+        rotation += rotationDirection;
+        if (rotation >= 360)
+        {
+            rotationDirection = -1;
+        }
+        else if (rotation <= 0)
+        {
+            rotationDirection = 1;
+        }
+
+        DrawFPS(10, 10);
         EndDrawing();
     }
-    
-    printf("Hello, World!\n");
+
+    printf("Size of Bool: %d\n", sizeof(bool));
+
     return 0;
 }
